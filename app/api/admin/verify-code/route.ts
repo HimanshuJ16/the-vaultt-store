f256faa6bf710d126b33c2179570aa863e4389d3
unknown <himanshujangir16@gmail.com>
2025-07-24 17:43:41 +0530

    first commit

--- app/api/admin/verify-code/route.ts
+++ app/api/admin/verify-code/route.ts
@@ -15,7 +15,7 @@ export async function POST(req: Request) {
       response.cookies.set('admin-authenticated', 'true', {
         httpOnly: true,
         secure: process.env.NODE_ENV === 'production',
-        maxAge: 60 * 60 * 24, // 1 day
+        maxAge: 60 * 60 * 24 * 30, // 30 day
         path: '/',
       });
 
